cd14392a35bd033caac462cfde5be223790d59f7
valeriö <valeriodambrosio.14@gmail.com>
2020-06-03 14:40:17 +0200

    Appunti

--- myscript.js
+++ myscript.js
@@ -5,3 +5,23 @@
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 // BONUS: all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali.
 // Con difficoltà 0=> tra 1 e 100, con difficoltà 1 => tra 1 e 80, con difficoltà 2=> tra 1 e 50
+
+//per 16 volte
+for (var i = 1; i <= 16; i++) {
+
+  i;
+
+  console.log(i);
+
+}
+
+//genera un numero compreso tra il valore 1 e il valore 100 (estremi inclusi)
+function fromUpTo(min, max) {
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+var numeroCasuale = fromUpTo(1, 100);
+
+console.log(numeroCasuale);
