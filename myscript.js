f44058a266685ddd2a1bb89c9af49d613c00cdca
valeriö <valeriodambrosio.14@gmail.com>
2020-06-04 15:13:33 +0200

    Post-correzione

--- myscript.js
+++ myscript.js
@@ -6,57 +6,103 @@
 // BONUS: all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali.
 // Con difficoltà 0=> tra 1 e 100, con difficoltà 1 => tra 1 e 80, con difficoltà 2=> tra 1 e 50
 
-//creo un array di 16 numeri random (ma univoci) compresi fra 1 e 100
-var bombs = [];
-
-while (bombs.length < 16) {
-
-  var numCasuale = fromUpTo(1, 100);
-
-  var numDoppio = bombs.includes(numCasuale)
-
-  if (!numDoppio) {
-
-   bombs.push(numCasuale);
-
-  }
-
-}
-
-console.log(bombs);
-
-var isBomb = false;
-var numScelto = parseInt(prompt("inserisci un numero"));
-
-for (var i = 0; i < bombs.length; i++) {
-
-
-  if (bombs[i] === numScelto) {
-
-    isBomb = true;
-
-  }
-
-  if (isBomb === true) {
-
-    alert("hai perso");
-
-  } else {
-
-    alert("bravo");
-  }
-
-
-}
-
-
-
-
-
-
+//INIZIO FUNZIONI\\
 //questa funzione genera un numero casuale compreso tra due valori (estremi inclusi)
-function fromUpTo(min, max) { //note: riutilizzabile quando vorrò aumentare la difficoltà del gioco
+function fromUpTo(min, max) {
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+//\
+
+//questa funzione crea un array di 16 mine in cui ogni mina è un numero univoco da 1 a 100)
+function creaArrayMine() {
+
+  ///array da riempire
+  var numeriMine = [];
+
+  ///loop finché array non è riempito
+  while (numeriMine.length < 16) {
+
+    ////criteri per riempire:
+    ////- numero casuale da 1 a 100
+    var numCasuale = fromUpTo(1, 100);
+
+    ////- solo se non è già presente nell' array
+    if ( checkDoppi(numCasuale, numeriMine) === false ) {
+
+      /////metodo per riempire
+      numeriMine.push(numCasuale);
+
+    }
+
+  }
+
+  ///ritorno array riempito
+  console.log(numeriMine);
+  return numeriMine;
+
+}
+//\
+
+//questa funzione controlla se un elemento è già presente in un array,
+//è simile a .includes()
+//ritorna un booleano: true = giappresente
+///////////////////////false = nompresente
+function checkDoppi(elementoDaVerificare, listaDaVagliare) {
+
+  //variabile di appoggio
+  var inArray = false;
+
+  //loop per tutta la lunghezza della lista
+  for (var i = 0; i < listaDaVagliare.length; i++) {
+
+    //se l'elemento da verificare è uguale a uno di quelli già presenti in lista
+    if (elementoDaVerificare === listaDaVagliare[i]) {
+
+      //allora l'elemento è già presente
+      inArray = true;
+
+    }
+
+  }
+
+  console.log(inArray);
+  return inArray;
+
+}
+//\
+//\\FINE FUNZIONI//
+
+
+//GIOCO CAMPO MINATO
+var arrayMine = creaArrayMine();
+var numeroImmissioniUtente = 0;
+var isMina = false;
+
+while ( (isMina === false) && (numeroImmissioniUtente < 84) ) {
+
+  var numUtente = parseInt(prompt("scegli un numero da 1 a 100"));
+
+  if(checkDoppi(numUtente, arrayMine)) {
+
+    isMina = true;
+
+  }
+
+  numeroImmissioniUtente++;
+
+  console.log("hai scelto " + numUtente);
+
+}
+
+if(isMina === false) {
+
+  console.log("vittoria!");
+
+} else {
+
+  console.log("boom!" + " il " + numUtente + " era una mina!");
+  console.log( "hai perso... " + "il tuo punteggio è " + parseInt((numeroImmissioniUtente - 1)) );
 
 }
